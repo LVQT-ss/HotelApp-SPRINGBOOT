9875fae5fe454d75d6100cab1c02b120864db8c7
LVQT <levietquocthinh@gmail.com>
2024-06-06 21:11:20 +0700

    add entity Room and BookedRoom

--- src/main/java/com/thinhle/lakesidehotel/model/BookedRoom.java
+++ src/main/java/com/thinhle/lakesidehotel/model/BookedRoom.java
@@ -1,4 +1,72 @@
 package com.thinhle.lakesidehotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate check_In_Date;
+
+    @Column(name = "check_Out")
+    private LocalDate check_Out_Date;
+
+    @Column(name = "Guest_FullName")
+    private String guestFullname;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    @Column(name = "children")
+    private int NumOfAdults;
+
+    @Column(name = "children")
+    private int NumOfChildren;
+
+    @Column(name = "total_guest")
+    private int totalNumberOfGuest;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @Column(name = "Room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumberOfGuest = this.NumOfAdults + this.NumOfChildren;
+    }
+
+
+    public void setNumOfChildren(int numOfChildren) {
+        NumOfChildren = numOfChildren;
+        calculateTotalNumberOfGuest();
+    }
+
+
+
+    public void setNumOfAdults(int numOfAdults) {
+        NumOfAdults = numOfAdults;
+        calculateTotalNumberOfGuest();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
+
 }
