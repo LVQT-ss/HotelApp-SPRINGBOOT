3bfcb2c7ea7fcb9879673f11524df3f5b9e82536
LVQT <levietquocthinh@gmail.com>
2024-06-17 23:17:01 +0700

    add addRoomService

--- src/main/java/com/thinhle/lakesidehotel/repository/RoomRepository.java
+++ src/main/java/com/thinhle/lakesidehotel/repository/RoomRepository.java
@@ -1,4 +1,7 @@
 package com.thinhle.lakesidehotel.repository;
 
-public interface RoomRepository {
+import com.thinhle.lakesidehotel.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomRepository extends JpaRepository<Room, Integer> {
 }
--- src/main/java/com/thinhle/lakesidehotel/service/IRoomService.java
+++ src/main/java/com/thinhle/lakesidehotel/service/IRoomService.java
@@ -1,4 +1,12 @@
 package com.thinhle.lakesidehotel.service;
 
+import com.thinhle.lakesidehotel.model.Room;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
 public interface IRoomService {
+    Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) throws SQLException, IOException;
 }
