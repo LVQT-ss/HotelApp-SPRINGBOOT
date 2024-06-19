e231ccfd39c3f584a0e3fa9aea098682c288b2f3
LVQT <levietquocthinh@gmail.com>
2024-06-20 02:06:57 +0700

    Added BookingService and method getallBookedRoom and added Exception Message for site

--- src/main/java/com/thinhle/lakesidehotel/controller/RoomController.java
+++ src/main/java/com/thinhle/lakesidehotel/controller/RoomController.java
@@ -1,9 +1,12 @@
 package com.thinhle.lakesidehotel.controller;
 
+import com.thinhle.lakesidehotel.model.BookedRoom;
 import com.thinhle.lakesidehotel.model.Room;
 import com.thinhle.lakesidehotel.response.RoomResponse;
+import com.thinhle.lakesidehotel.service.BookingService;
 import com.thinhle.lakesidehotel.service.IRoomService;
 import lombok.RequiredArgsConstructor;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -21,7 +25,7 @@ import java.util.List;
 public class RoomController {
 
     private final IRoomService roomService;
-
+    private final BookingService bookingService;
     @PostMapping("/add/new-room")
     public ResponseEntity<RoomResponse> addNewRoom(
             @RequestParam("photo") MultipartFile photo,
@@ -37,5 +41,28 @@ public class RoomController {
     @GetMapping("/room/types")
     public List<String> getRoomTypes(){
         return roomService.getAllRoomTypes();
+    }
+
+    public ResponseEntity<List<RoomResponse>> getAllRooms() throws SQLException {
+        List<Room> rooms = roomService.getAllRooms();
+        List<RoomResponse> roomResponses = new ArrayList<>();
+        for (Room room : rooms) {
+            byte[] photoBytes = roomService.getRoomPhotoByRoomId(room.getId());
+            if(photoBytes != null && photoBytes.length > 0) {
+                String base64Photo = Base64.encodeBase64String(photoBytes);
+                RoomResponse  roomResponse = getRoomResponse(room);
+                roomResponse.setPhoto(base64Photo);
+                roomResponses.add(roomResponse);
+            }
+        }
+        return ResponseEntity.ok(roomResponses);
+    }
+
+    private RoomResponse getRoomResponse(Room room) {
+        List<BookedRoom> boookings = getAllBookingsByRoomId(room.getId());
+
+    }
+    private List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
+        return bookingService.getAllBookingsByRoomId(roomId);
     }
 }
--- src/main/java/com/thinhle/lakesidehotel/service/RoomService.java
+++ src/main/java/com/thinhle/lakesidehotel/service/RoomService.java
@@ -1,8 +1,10 @@
 package com.thinhle.lakesidehotel.service;
 
+import com.thinhle.lakesidehotel.exception.ResourceNotFoundException;
 import com.thinhle.lakesidehotel.model.Room;
 import com.thinhle.lakesidehotel.repository.RoomRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -12,6 +14,7 @@ import java.math.BigDecimal;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -36,5 +39,23 @@ public class RoomService implements IRoomService {
     @Override
     public List<String> getAllRoomTypes() {
         return roomRepository.findDistinctRoomTypes();
+    }
+
+    @Override
+    public List<Room> getAllRooms() {
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public byte[] getRoomPhotoByRoomId(Long roomId) throws SQLException {
+        Optional<Room> theRoom = roomRepository.findById(roomId);
+        if(theRoom.isEmpty()){
+            throw new ResourceNotFoundException("Sorry , Room not fuond");
+        }
+        Blob photoBlob = theRoom.get().getPhoto();
+        if(photoBlob != null){
+            return photoBlob.getBytes(1,(int) photoBlob.length());
+        }
+        return null;
     }
 }
