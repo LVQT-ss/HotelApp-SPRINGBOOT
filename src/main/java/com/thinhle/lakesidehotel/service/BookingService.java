5b0c40c8cf98f544fe34d2d902edaa1b731c9a8e
LVQT <levietquocthinh@gmail.com>
2024-06-22 14:37:27 +0700

    add Service for all method

--- src/main/java/com/thinhle/lakesidehotel/service/BookingService.java
+++ src/main/java/com/thinhle/lakesidehotel/service/BookingService.java
@@ -1,12 +1,15 @@
 package com.thinhle.lakesidehotel.service;
 
 
+import com.thinhle.lakesidehotel.exception.InvalidBookingResquestException;
 import com.thinhle.lakesidehotel.model.BookedRoom;
+import com.thinhle.lakesidehotel.model.Room;
 import com.thinhle.lakesidehotel.repository.BookingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,12 +22,12 @@ public class BookingService implements IBookingService {
 
     @Override
     public List<BookedRoom> getAllBookings() {
-        return null;
+        return bookingRepository.findAll();
     }
 
-        public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
-            return null;
-        }
+    public List<BookedRoom> getAllBookingsByRoomId(Long roomId) {
+            return bookingRepository.findByRoomId(roomId);
+    }
 
 
     @Override
@@ -34,12 +37,48 @@ public class BookingService implements IBookingService {
 
     @Override
     public String saveBooking(Long roomId, BookedRoom bookingRequest) {
-        return "";
+        if(bookingRequest.getCheck_Out_Date().isBefore(bookingRequest.getCheck_In_Date())) {
+            throw new InvalidBookingResquestException("Check-in date must come before check out date");
+        }
+        Room room = roomService.getRoomById(roomId).get();
+        List<BookedRoom> existingbookings = room.getBookings();
+        boolean roomIsAvailable = roomIsAvailable(bookingRequest,existingbookings);
+        if(roomIsAvailable) {
+
+            room.addBooking(bookingRequest);
+            bookingRepository.save(bookingRequest);
+        }else {
+                throw new InvalidBookingResquestException("sorry, This room is not available for the selected dates");
+        }
+
+        return bookingRequest.getBookingConfirmationCode();
+    }
+
+    private boolean roomIsAvailable(BookedRoom bookingRequest, List<BookedRoom> existingBookings) {
+        return existingBookings.stream()
+                .noneMatch(existingBooking ->
+                        bookingRequest.getCheck_In_Date().equals(existingBooking.getCheck_In_Date())
+                                || bookingRequest.getCheck_Out_Date().isBefore(existingBooking.getCheck_Out_Date())
+                                || (bookingRequest.getCheck_In_Date().isAfter(existingBooking.getCheck_In_Date())
+                                && bookingRequest.getCheck_In_Date().isBefore(existingBooking.getCheck_Out_Date()))
+                                || (bookingRequest.getCheck_In_Date().isBefore(existingBooking.getCheck_In_Date())
+
+                                && bookingRequest.getCheck_Out_Date().equals(existingBooking.getCheck_Out_Date()))
+                                || (bookingRequest.getCheck_In_Date().isBefore(existingBooking.getCheck_In_Date())
+
+                                && bookingRequest.getCheck_Out_Date().isAfter(existingBooking.getCheck_Out_Date()))
+
+                                || (bookingRequest.getCheck_In_Date().equals(existingBooking.getCheck_Out_Date())
+                                && bookingRequest.getCheck_Out_Date().equals(existingBooking.getCheck_In_Date()))
+
+                                || (bookingRequest.getCheck_In_Date().equals(existingBooking.getCheck_Out_Date())
+                                && bookingRequest.getCheck_Out_Date().equals(bookingRequest.getCheck_In_Date()))
+                );
     }
 
     @Override
-    public BookedRoom findByBookingConfirmationCode(String confirmationCode) {
-        return null;
+    public Optional<BookedRoom> findByBookingConfirmationCode(String confirmationCode) {
+        return bookingRepository.findByBookingConfirmationCode(confirmationCode);
     }
 
 }
--- src/main/java/com/thinhle/lakesidehotel/service/IBookingService.java
+++ src/main/java/com/thinhle/lakesidehotel/service/IBookingService.java
@@ -3,13 +3,14 @@ package com.thinhle.lakesidehotel.service;
 import com.thinhle.lakesidehotel.model.BookedRoom;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface IBookingService {
     void cancelBooking(Long bookingId);
 
     String saveBooking(Long roomId, BookedRoom bookingRequest);
 
-    BookedRoom findByBookingConfirmationCode(String confirmationCode);
+    Optional<BookedRoom> findByBookingConfirmationCode(String confirmationCode);
 
     List<BookedRoom> getAllBookings();
 }
