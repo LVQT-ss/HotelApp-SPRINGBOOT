7b87be74558528aa9327185fce2fdf1b69e38a2f
LVQT <levietquocthinh@gmail.com>
2024-08-05 00:23:55 +0700

    add hotel user serviece

---
+++ src/main/java/com/thinhle/lakesidehotel/security/user/HotelUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.thinhle.lakesidehotel.security.user;
+
+import com.thinhle.lakesidehotel.model.User;
+import com.thinhle.lakesidehotel.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class HotelUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return HotelUserDetails.buildUserDetails(user);
+    }
+
+
+}
