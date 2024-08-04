ff02112c94510de9b57c14316f9b368fff2280e2
LVQT <levietquocthinh@gmail.com>
2024-08-04 16:12:33 +0700

    add remove role for user and assisgn

--- src/main/java/com/thinhle/lakesidehotel/service/RoleService.java
+++ src/main/java/com/thinhle/lakesidehotel/service/RoleService.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -39,21 +40,41 @@ public class RoleService implements IRoleService {
 
     @Override
     public Role findByName(String name) {
-        return null;
+        return roleRepository.findByName(name).get();
     }
+
 
     @Override
     public User removeUserFromRole(Long userId, Long roleId) {
-        return null;
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Role>  role = roleRepository.findById(roleId);
+        if (role.isPresent() && role.get().getUsers().contains(user.get())){
+            role.get().removeUserFromRole(user.get());
+            roleRepository.save(role.get());
+            return user.get();
+        }
+        throw new UsernameNotFoundException("User not found");
     }
 
     @Override
     public User assignRoleToUser(Long userId, Long roleId) {
-        return null;
+        Optional<User> user = userRepository.findById(userId);
+        Optional<Role>  role = roleRepository.findById(roleId);
+        if (user.isPresent() && user.get().getRoles().contains(role.get())){
+            throw new UserAlreadyExistsException(
+                    user.get().getFirstName()+ " is already assigned to the" + role.get().getName()+ " role");
+        }
+        if (role.isPresent()){
+            role.get().assignRoleToUser(user.get());
+            roleRepository.save(role.get());
+        }
+        return user.get();
     }
 
     @Override
     public Role removeAllUsersFromRole(Long roleId) {
-        return null;
+        Optional<Role> role = roleRepository.findById(roleId);
+        role.ifPresent(Role::removeAllUsersFromRole);
+        return roleRepository.save(role.get());
     }
 }
